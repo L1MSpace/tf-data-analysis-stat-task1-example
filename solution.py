586c58659e35d3e94f9dc17244d93429f5edd582
L1MSpace <129736244+L1MSpace@users.noreply.github.com>
2023-04-05 18:37:21 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,8 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 704471350 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    x = sum(x) / len(x)+18
+    return x # Ваш ответ
