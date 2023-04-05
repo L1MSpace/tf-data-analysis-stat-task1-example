f396d9fdc2d20b439e1a267f5e8dd6cae705857a
L1MSpace <129736244+L1MSpace@users.noreply.github.com>
2023-04-05 18:47:56 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,9 +1,8 @@
 import pandas as pd
 import numpy as np
 
-
-chat_id = 704471350 # Ваш chat ID, не меняйте название переменной
+chat_id = 704471350
 
 def solution(x: np.array) -> float:
-    x = sum(x) / len(x)+18
-    return x # Ваш ответ
+    x = sum(x) / (len(x)+18)
+    return x
